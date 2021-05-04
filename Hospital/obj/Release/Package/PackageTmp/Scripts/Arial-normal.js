cf552142b131830227547db728403ccb5c10b6bf
Abdulrahman Almabadi <aalmobdi001@stu.kau.edu.sa>
2021-05-05 02:00:45 +0300

    up

--- Hospital/obj/Release/Package/PackageTmp/Scripts/Arial-normal.js
+++
@@ -1,8 +0,0 @@
-﻿(function (jsPDFAPI) {
-var font = 'undefined';
-var callAddFont = function () {
-this.addFileToVFS('Arial-normal.ttf', font);
-this.addFont('Arial-normal.ttf', 'Arial', 'normal');
-};
-jsPDFAPI.events.push(['addFonts', callAddFont])
- })(jsPDF.API);
